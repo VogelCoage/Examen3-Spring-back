7f6590ca9e85d4fa627afcdb1012d86aaae95e73
VogelCoage <bladburn@gmail.com>
2021-04-11 22:14:49 -0500

    Segundo commit - v0.1 - hola mundo

---
+++ src/main/java/uia/com/api/ContabilidadUIA/HomeController.java
@@ -0,0 +1,13 @@
+package uia.com.api.ContabilidadUIA;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/home")
+	public String hello() {
+		return "Hola mundo, usando RESTful con Spring Boot";
+	}
+
+}
